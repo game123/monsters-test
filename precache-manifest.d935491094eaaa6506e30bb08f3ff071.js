825ff383d86de1c481033afd694ae2ecfa057f5b
Kevin Chan <kevinykchan@gmail.com>
2019-11-12 16:09:22 -0500

    Updates

---
+++ precache-manifest.d935491094eaaa6506e30bb08f3ff071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994ec29807080fb0199e672c1da772bd",
+    "url": "/monsters-test/index.html"
+  },
+  {
+    "revision": "c4d674e7e61a8d85c433",
+    "url": "/monsters-test/static/css/main.2fb2a089.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-test/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "c4d674e7e61a8d85c433",
+    "url": "/monsters-test/static/js/main.639de170.chunk.js"
+  },
+  {
+    "revision": "78c499f01638419db954",
+    "url": "/monsters-test/static/js/runtime-main.b1eddf15.js"
+  }
+]);
